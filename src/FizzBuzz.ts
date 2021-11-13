049fa032f6408aa13396121bc823dc350c767935
Kat <Katrinabefus@gmail.com>
2021-11-13 11:34:41 -0600

    Notes Review

--- src/FizzBuzz.ts
+++ src/FizzBuzz.ts
@@ -1,9 +1,11 @@
 import { hasADigit } from "./NumberHelpers";
 
 export function fizzbuzz(n:number):string {
+    //if the remainder of n/3 is equal to 0 and if the remainder of n/5 is equal to 0
     if (n % 3 === 0 && n % 5 === 0) {
         return "FizzBuzz";
     }
+    //if the remainder of n/3 is equal to 0 or n has a 3 as one of the digits 
     if (n % 3 === 0 || hasADigit(n, 3)) {
         return "Fizz";
     }
@@ -12,6 +14,3 @@ export function fizzbuzz(n:number):string {
     }
     return n.toString();
 }
-
-
-
--- src/NumberHelpers.ts
+++ src/NumberHelpers.ts
@@ -1,4 +1,7 @@
+                        // n=parameter :number=type     ):boolean=what should be returned
 export function hasADigit(n:number, numberToFind:number):boolean{
+                            //returns a string version of an object
     var numberAsAString = n.toString();
+                        //searches for an occurance of numberToFind in numberAsAString
     return numberAsAString.indexOf(numberToFind.toString()) > -1;
 }
